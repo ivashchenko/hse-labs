8c631610b114f3818048e1a2b9482a29aa97dfbb
ivashchenko <ivashchenkoov@gmail.com>
2021-02-28 23:21:04 +0300

    Работает параллельное fft !

--- FftLab2/FftLab2/FFT.cs
+++ FftLab2/FftLab2/FFT.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Numerics;
 using System.Text;
 using System.Threading;
@@ -17,6 +18,7 @@ namespace FftLab2
             return new Complex(Math.Cos(arg), Math.Sin(arg));
         }
 
+        // Исходная функция FFT (использует рекурсивный подход "сверху-вниз")
         public static Complex[] fft(Complex[] x)
         {
             Complex[] X;
@@ -48,9 +50,8 @@ namespace FftLab2
             return X;
         }
 
-        static int counter = 0;
-
-        public static Complex[] pfft(Complex[] x, int level, int forkLevel)
+        // параллельная fft, 2х раза быстрее
+        public static Complex[] pfft(Complex[] x, int level=1, int forkLevel=1)
         {
             Complex[] X;
             int N = x.Length;
@@ -93,100 +94,109 @@ namespace FftLab2
             return X;
         }
 
-        public static Complex[] p2fft(Complex[] x, int maxThreads, int level = 1)
+        // инверсия битов числа int
+        public static int Reverse(int value, int high_bit)
         {
-            Complex[] X;
-            int N = x.Length;
-            if (N == 2)
-            {
-                X = new Complex[2];
-                X[0] = x[0] + x[1];
-                X[1] = x[0] - x[1];
-            }
-            else
-            {
-                Complex[] x_even = new Complex[N / 2];
-                Complex[] x_odd = new Complex[N / 2];
-                for (int i = 0; i < N / 2; i++)
-                {
-                    x_even[i] = x[2 * i];
-                    x_odd[i] = x[2 * i + 1];
+            int one = 1, result = 0;
 
-                }
-                Complex[] X_even = null, X_odd = null;
-                if (false)
-                {
-                    X_even = p2fft(x_even, maxThreads, level + 1);
-                    X_odd = p2fft(x_odd, maxThreads, level + 1);
+            do
+            {
+                if ((value & one) > 0)
+                    result |= high_bit;
+                high_bit >>= 1;
+                one <<= 1;
+            } while (high_bit != 0);
 
-                    Parallel.For(0, maxThreads, p =>
+            return result;
+        }
+
+        // FFT снизу-вверх, без рекурсии
+        public static Complex[] fft2(Complex[] x)
+        {
+            int N = x.Length, high_bit = N / 2;
+            Complex[] X = new Complex[N];
+            Complex[] X2 = new Complex[N];
+
+            for (int n = 2; n <= N; n *= 2)
+            {
+                for (int i = 0; i < N; i += n)
+                {
+                    for (int j = i; j < i + n / 2; j++)
                     {
-                        int n = N / 2;
-                        int M = n / maxThreads, m = n % maxThreads;
-                        int max = p == maxThreads - 1 ? M + m : M;
-
-                        for (int j = 0, i = p * M; j < max; j++, i++)
+                        if (n == 2)
                         {
-                            X[i] = X_even[i] + w(i, N) * X_odd[i];
-                            X[i + N / 2] = X_even[i] - w(i, N) * X_odd[i];
+                            int jj = Reverse(j, high_bit);
+                            int jj1 = Reverse(j + 1, high_bit);
+                            X2[j] = x[jj] + x[jj1];
+                            X2[j + 1] = x[jj] - x[jj1];
                         }
-                    });
-                }
-                else
-                {
-                    int myId = 0;
-                    if (level < 4) 
-                    {
-                        myId = Interlocked.Increment(ref counter);
-                        Console.WriteLine($"{myId} ({level}) born {DateTime.Now.Second}:{DateTime.Now.Millisecond}");
-                    }
-
-                    if (level <= 2)
-                    {
-                        int nextlevel = level + 1;
-                        Parallel.Invoke(
-                            () => X_even = p2fft(x_even, maxThreads, nextlevel),
-                            () => X_odd = p2fft(x_odd, maxThreads, nextlevel));
-
-                        X = new Complex[N];
-
-                        //Console.WriteLine("done");
-                        Parallel.For(0, 2, p =>
+                        else
                         {
-                            int n = N / 2;
-                            int M = n / 2;
+                            X2[j] = X[j] + w(j, n) * X[j + n / 2];
+                            X2[j + n / 2] = X[j] - w(j, n) * X[j + n / 2];
+                        }
+                    }
+                }
+                (X, X2) = (X2, X);
+            }
+            return X;
+        }
 
-                            for (int j = 0, i = p * M; j < M; j++, i++)
+        // параллельная версия FFT снизу-вверх, без рекурсии
+        public static Complex[] pfft2(Complex[] x, int maxThreads)
+        {
+            int N = x.Length, high_bit = N / 2;
+            Complex[] X = new Complex[N];
+            Complex[] X2 = new Complex[N];
+
+            maxThreads = Math.Min(maxThreads, x.Length / 2);
+            int chunk = N / maxThreads;
+            Barrier _barrier = new Barrier(maxThreads);
+
+            Parallel.For(0, maxThreads, tid => {
+
+                int start = tid * chunk,
+                    end = tid == (maxThreads - 1) ? N : (tid + 1) * chunk;
+
+                for (int n = 2; n <= N; n *= 2)     // размер блока ("бабочки")
+                {
+                    for (int i = 0; i < N; i += n)  // номер первого элемента в блоке
+                    {
+                        if (i >= end || (i + n) < start) continue;  // границы потока не пересекают блок. пропускаем!
+
+                        for (int j = i; j < i + n / 2; j++) // индекс элемента блока
+                        {
+                            if (j < start || j >= end) continue;
+                            if (n == 2)
                             {
-                                X[i] = X_even[i] + w(i, N) * X_odd[i];
-                                X[i + N / 2] = X_even[i] - w(i, N) * X_odd[i];
+                                int jj = Reverse(j, high_bit);
+                                int jj1 = Reverse(j + 1, high_bit);
+                                X2[j] = x[jj] + x[jj1];
                             }
-                        });
-                    }
-                    else
-                    {
-                        int nextlevel = level + 1;
-                        if (level == 3)
-                            Console.WriteLine($"n={N}");
+                            else
+                                X2[j] = X[j] + w(j, n) * X[j + n / 2];
+                        }
 
-                        //X_even = p2fft(x_even, maxThreads, nextlevel);
-                        //X_odd = p2fft(x_odd, maxThreads, nextlevel);
-
-                        X_even = fft(x_even);
-                        X_odd = fft(x_odd);
-
-                        X = new Complex[N];
-                        for (int i = 0; i < N / 2; i++)
+                        for (int j = i + n / 2; j < i + n; j++)
                         {
-                            X[i] = X_even[i] + w(i, N) * X_odd[i];
-                            X[i + N / 2] = X_even[i] - w(i, N) * X_odd[i];
+                            if (j < start || j >= end) continue;
+                            if (n == 2)
+                            {
+                                int jj = Reverse(j, high_bit);
+                                int jj1 = Reverse(j - 1, high_bit);
+                                X2[j] = x[jj1] - x[jj];
+                            }
+                            else
+                                X2[j] = X[j - n / 2] - w(j - n / 2, n) * X[j];
                         }
                     }
-                    string space = " ";
-                    if(myId > 0)
-                        Console.WriteLine($"{space:level}{myId} dead {DateTime.Now.Second}:{DateTime.Now.Millisecond}");
+                    _barrier.SignalAndWait();   // дождаться пока ВСЕ потоки закончат этот уровень                    
+                    if (tid == 0)
+                        (X, X2) = (X2, X);
+                    _barrier.SignalAndWait();   // ещё раз подождать
                 }
-            }
+            });
+
             return X;
         }
     }
--- FftLab2/FftLab2/Program.cs
+++ FftLab2/FftLab2/Program.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Collections;
 
 namespace FftLab2
 {
@@ -19,34 +20,53 @@ namespace FftLab2
             return new Complex(Math.Cos(arg), Math.Sin(arg));
         }
 
+        static double error(Complex[] signal1, Complex[] signal2)
+        {
+            double res = 0;
+            for (int i = 0; i < signal1.Length; i++)
+            {
+                double e = (signal1[i] - signal2[i]).Magnitude;
+                res += e*e;
+            }
+            return Math.Sqrt(res / signal1.Length);
+        }
+
         static void Main(string[] args)
         {
-            int N = (int)Math.Pow(2, 24);
+            int P = 24, N = (int)Math.Pow(2, P);
             Stopwatch sw = new Stopwatch();
-
             Complex[] signal = new Complex[N];
 
             for (int t = 0; t < signal.Length; t++)
                 signal[t] = Math.Sin(2 * Math.PI * t / 1000);
+                //signal[t] = t % 100;
 
             sw.Restart();
-            var spectre1 = FFT.fft(signal);
+            Complex[] sp1 = FFT.fft(signal);
             sw.Stop();
-            Console.WriteLine($"1'st: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"1'st fft(N=2^{P}): {sw.ElapsedMilliseconds} ms");
 
             sw.Restart();
-            int forkLevel = (int)Math.Log2(8);
-            var spectre2 = FFT.p2fft(signal, 4);
+            int forkLevel = (int)Math.Log2(Environment.ProcessorCount);
+            var sp2 = FFT.pfft(signal, 1, forkLevel);
             sw.Stop();
-            Console.WriteLine($"2'd: {sw.ElapsedMilliseconds} ms");
-            Console.WriteLine($"Signals are equal: {spectre1.SequenceEqual(spectre2)}");
+            Console.WriteLine($"2'd pfft: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Signals are equal: {sp1.SequenceEqual(sp2)}, error={error(sp1, sp2)}");
 
             sw.Restart();
-            var spectre3 = FFT.pfft(signal, 1, forkLevel);
+            Complex[] sp3 = FFT.fft2(signal);
             sw.Stop();
-            Console.WriteLine($"3'd: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"3'd fft2(N=2^{P}): {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Signals are equal: {sp1.SequenceEqual(sp3)}, error={error(sp1, sp3)}");
 
-            Console.WriteLine($"Signals are equal: {spectre1.SequenceEqual(spectre3)}");
+            Console.WriteLine($"4'th pfft2 loop:");
+            for (int i = 1; i <= Environment.ProcessorCount; i++)
+            {
+                sw.Restart();
+                Complex[] sp4 = FFT.pfft2(signal, i);
+                sw.Stop();
+                Console.WriteLine($"Threads={i} done {sw.ElapsedMilliseconds} ms, signals are equal: {sp1.SequenceEqual(sp2)}, error = {error(sp3, sp4)}");
+            }
         }
     }
 }
